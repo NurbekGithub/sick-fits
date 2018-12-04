d72bcbb692116de85df1c7e13639091539b60b4e
Nurbek <nurbekizbassar@gmail.com>
2018-12-04 23:09:00 +0500

    Fix: fixed untracked files

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,19 @@
 import { Nav } from "./Nav";
 import Link from 'next/link';
 import styled from 'styled-components';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+}
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+}
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+}
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -31,9 +44,11 @@ const StyledHeader = styled.header`
       grid-template-columns: 1fr;
       justify-content: center;
     }
-    .sub-bar {
-      
-    }
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid ${props => props.theme.lightgrey}
   }
 `
 
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,12 +1,21 @@
 import Link from 'next/link';
-
+import StyledNav from './styles/NavStyles';
 export const Nav = props => (
-    <nav>
-        <Link href='/'>
-            <a>Home!</a>
+    <StyledNav>
+        <Link href='/items'>
+            <a>Items</a>
         </Link>
         <Link href='/sell'>
-            <a>Sell!</a>
+            <a>Sell</a>
         </Link>
-    </nav>
+        <Link href='/signup'>
+            <a>Signup</a>
+        </Link>
+        <Link href='/orders'>
+            <a>Orders</a>
+        </Link>
+        <Link href='/me'>
+            <a>Accound</a>
+        </Link>
+    </StyledNav>
 )
